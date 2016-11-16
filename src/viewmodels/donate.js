f111c055478c06220100901ddc6b940fcf45f084
John Kennedy <johnwjken@gmail.com>
2016-11-16 11:53:35 +0000

    Imported basic donation functionality to the web app

---
+++ src/viewmodels/donate.js
@@ -0,0 +1,23 @@
+export class Donate {
+
+  amount = 5;
+  methods = ['Cash', 'PayPal'];
+  selectedMethod = 'Cash';
+
+  candidates = [
+    {
+      firstName: 'Lisa',
+      lastName: 'Simpson'
+    },
+    {
+      firstName: 'Bart',
+      lastName: 'Simpson'
+    }
+  ];
+  selectedCandidate = this.candidates[0];
+
+  makeDonation() {
+    console.log(`Amount = ${this.amount}`);
+    console.log(`Method = ${this.selectedMethod}`);
+  }
+}
